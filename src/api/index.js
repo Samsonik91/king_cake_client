5673e148acd5cbda0e51a2d85d50110ba059d83f
Samsonik91 <111237284+Samsonik91@users.noreply.github.com>
2022-11-06 21:47:12 +0200

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const isProd = (process.env.NODE_ENV === 'production')
-const URL = isProd ? "https://king-cakes.herokuapp.com/" : "http://localhost:5000"
+const URL = isProd ? "https://cake.herokuapp.com/" : "http://localhost:5000"
 
 const Api = axios.create({
     baseURL: URL
